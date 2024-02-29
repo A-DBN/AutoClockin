10c60c20f9f05e61c99fddd0b5478d7769e9bb47
A-DBN-PI <adabin@hotmail.fr>
2024-03-01 01:09:13 +0900

    [~] Send new version on git to work remotly

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.firefox.service import Service
+from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+import time
+
 
 credentials = {
     1: ('username1', 'password1'),
@@ -16,43 +18,54 @@ credentials = {
 LOGIN_URL = 'https://epitech.bamboohr.com/login.php'
 USERNAME_FIELD_ID = 'lemail'
 PASSWORD_FIELD_ID = 'password'
-LOGIN_BUTTON_XPATH = '//*[@id="passwordFields"]/div[3]/button'
-NEW_PAGE_BUTTON_XPATH = '//*[@id="TIME_TRACKING"]/div/div[2]/button[1]'
+LOGIN_BUTTON_TEXT = 'Log In'
+NEW_PAGE_BUTTON_TEXT_IN = 'Clock In'
+NEW_PAGE_BUTTON_TEXT_OUT = 'Clock Out'
 
 def login_with_credentials(user_id):
     # Initialize WebDriver options
-    chrome_options = Options()
-    chrome_options.binary_location = '/usr/bin/chromium-browser'  # Path to Chromium
-    chrome_options.add_argument("--headless")  # Optional: Run in headless mode
+    firefox_options = Options()
+    firefox_options.binary_location = '/usr/bin/firefox-esr'
+    firefox_options.add_argument("--headless")  # Optional: Run in headless mode
 
     # Setup WebDriver
-    service = Service(executable_path='/usr/bin/chromedriver')  # Path to ChromeDriver
-    driver = webdriver.Chrome(service=service, options=chrome_options)
+    service = Service(executable_path='/usr/local/bin/geckodriver')  # Path to geckodriver
+    driver = webdriver.Firefox(service=service, options=firefox_options)
     
     try:
         # Open the login page
         driver.get(LOGIN_URL)
         
         # Wait for the login input fields to be loaded
-        WebDriverWait(driver, 10).until(
-            EC.presence_of_element_located((By.ID, USERNAME_FIELD_ID))
-        )
+#        WebDriverWait(driver, 60).until(
+#            EC.visibility_of_element_located((By.ID, USERNAME_FIELD_ID))
+#        )
         
         # Get credentials for the given user ID
         username, password = credentials[user_id]
         
         # Perform login
-        driver.find_element(By.ID, USERNAME_FIELD_ID).send_keys(username)
+        username = driver.find_element(By.ID, USERNAME_FIELD_ID).send_keys(username)
+        print(username)
         driver.find_element(By.ID, PASSWORD_FIELD_ID).send_keys(password)
-        driver.find_element(By.XPATH, LOGIN_BUTTON_XPATH).click()
-        
+        button = driver.find_element(By.LINK_TEXT, LOGIN_BUTTON_TEXT)
+        print("Found out buttons")
+        print(button)
+        button.click()
+        time.sleep(10)
         # Wait for the new page to load
-        WebDriverWait(driver, 10).until(
-            EC.presence_of_element_located((By.XPATH, NEW_PAGE_BUTTON_XPATH))
-        )
-        
+#        WebDriverWait(driver, 60).until(
+#            EC.visibility_of_element_located((By.XPATH, NEW_PAGE_BUTTON_XPATH))
+#        )
+       
         # Click on the new button on the loaded page
+        print(driver.current_url)
         driver.find_element(By.XPATH, NEW_PAGE_BUTTON_XPATH).click()
-
+        print("CLick final")
     finally:
         driver.quit()
+
+# Replace '/path/to/geckodriver' with the actual path to your geckodriver executable.
+# If geckodriver is in your system PATH, you can omit the `executable_path` argument.
+
+login_with_credentials(1)  # Replace with the user ID you want to test
